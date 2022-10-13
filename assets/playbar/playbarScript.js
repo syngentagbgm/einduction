bd26023ab97e52db3509a624b1d7117ba353ddf0
syngentagbgm <108794459+syngentagbgm@users.noreply.github.com>
2022-10-13 14:37:23 +0100

    Update 13-10-22

--- assets/playbar/playbarScript.js
+++ assets/playbar/playbarScript.js
@@ -7,7 +7,6 @@ cp.playbarAssetArr =
 	'Color',
 	'ColorSmall',
 	'CC',
-	'EnterVR',
 	'Exit',
 	'FastForward',
 	'FastForward1',
@@ -34,7 +33,6 @@ cp.playbarTooltips =
 	Backward : 'Backward',
 	Color : 'Color',
 	CC : 'CC',
-	EnterVR : 'EnterVR',
 	Exit : 'Exit',
 	FastForward : '2x Fast Forward Speed',
 	FastForward1 : '4x Fast Forward Speed',
@@ -52,21 +50,6 @@ cp.playbarTooltips =
 	Thumb : 'Thumb',
 	ThumbBase : 'ThumbBase',
 	TOC : 'TOC'
-};
-cp.responsiveButtons = 
-{
-	//"ButtonName"	: 	[Primary,Tablet,Mobile],
-	"Rewind"		: 	[true,true,true,true,false],
-	"Backward"		: 	[true,true,true,true,true],
-	"Play"			: 	[true,true,true,true,true],
-	"Slider"		: 	[true,true,true,true,false],
-	"Forward"		: 	[true,true,true,true,true],
-	"CC"			: 	[true,true,true,true,true],
-	"AudioOn"		: 	[true,true,false,false,false],
-	"Exit"			: 	[true,true,true,true,true],
-	"EnterVR"		:   [true,true,true,true,true],
-	"FastForward"	: 	[true,true,true,true,false],
-	"TOC"			: 	[true,true,true,true,false]
 };
 cp.handleSpecialForPlaybar = function(playbarConstruct)
 {
